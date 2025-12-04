d058e761a8ff1d710ac6324db264d0f81e845232
belennhr <belenn.hr@gmail.com>
2025-12-04 10:42:21 +0100

    ProductosPage + ClientesPage

--- Views/ClientesPage.xaml.cs
+++ Views/ClientesPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,48 @@ namespace TVPGestion_IPO.Views
     /// </summary>
     public partial class ClientesPage : Page
     {
+        private ObservableCollection<ClienteViewModel> clientesVM;
+        private ICollectionView clientesView;
+
         public ClientesPage()
         {
             InitializeComponent();
+
+            // Ejemplo de datos
+            clientesVM = new ObservableCollection<ClienteViewModel>
+            {
+                new ClienteViewModel { Nombre = "Juan", Apellidos = "Pérez", DireccionesString = "Calle 1, Ciudad", TelefonosString = "123456789", EmailsString = "juan@mail.com", AlergiasString = "Ninguna", FormaPago = "Tarjeta", PuntosAcumulados = 100 },
+                new ClienteViewModel { Nombre = "Ana", Apellidos = "García", DireccionesString = "Avenida 2, Ciudad", TelefonosString = "987654321", EmailsString = "ana@mail.com", AlergiasString = "Gluten", FormaPago = "Bizum", PuntosAcumulados = 50 }
+                // ... más clientes
+            };
+
+            clientesView = CollectionViewSource.GetDefaultView(clientesVM);
+            ClientesDataGrid.ItemsSource = clientesView;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var filtro = SearchBox.Text?.ToLower() ?? "";
+            clientesView.Filter = item =>
+            {
+                var cli = item as ClienteViewModel;
+                return cli != null && (
+                    cli.Nombre.ToLower().Contains(filtro) ||
+                    cli.Apellidos.ToLower().Contains(filtro) ||
+                    cli.DireccionesString.ToLower().Contains(filtro) ||
+                    cli.TelefonosString.ToLower().Contains(filtro) ||
+                    cli.EmailsString.ToLower().Contains(filtro) ||
+                    cli.AlergiasString.ToLower().Contains(filtro) ||
+                    cli.FormaPago.ToLower().Contains(filtro) ||
+                    cli.PuntosAcumulados.ToString().Contains(filtro)
+                );
+            };
+            clientesView.Refresh();
+        }
+
+        private void BtnAddCliente_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Views/ProductosPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using TVPGestion_IPO.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TVPGestion_IPO.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ProductosPage.xaml
+    /// </summary>
+    public partial class ProductosPage : Page
+    {
+        private ObservableCollection<ProductoViewModel> productosVM;
+        private ICollectionView productosView;
+
+        public ProductosPage()
+        {
+            InitializeComponent();
+
+            // Ejemplo de datos
+            productosVM = new ObservableCollection<ProductoViewModel>
+            {
+                new ProductoViewModel { Nombre = "Pizza Margarita", Categoria = "Pizza", Subcategoria = "Clásica", Foto = "/Assets/Icons/comidaIcon.png", Precio = 8.99m, AlergenosString = "Gluten, Lácteos", IngredientesString = "Tomate, Queso, Albahaca" },
+                new ProductoViewModel { Nombre = "Hamburguesa", Categoria = "Hamburguesa", Subcategoria = "Especial", Foto = "/Assets/Icons/comidaIcon.png", Precio = 6.99m, AlergenosString = "Gluten", IngredientesString = "Carne, Queso, Pan" },
+                // ... más productos
+            };
+
+            productosView = CollectionViewSource.GetDefaultView(productosVM);
+            ProductosDataGrid.ItemsSource = productosView;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var filtro = SearchBox.Text?.ToLower() ?? "";
+            productosView.Filter = item =>
+            {
+                var prod = item as ProductoViewModel;
+                return prod != null && (
+                    prod.Nombre.ToLower().Contains(filtro) ||
+                    prod.Categoria.ToLower().Contains(filtro) ||
+                    prod.Subcategoria.ToLower().Contains(filtro) ||
+                    prod.AlergenosString.ToLower().Contains(filtro) ||
+                    prod.IngredientesString.ToLower().Contains(filtro)
+                );
+            };
+            productosView.Refresh();
+        }
+
+        private void BtnAddProducto_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
