9a08a51e1b0e23338f6dd415402eb4ed9bb57dc3
Álvaro Márquez Gavilán <Alvaro.Marquez1@alu.uclm.es>
2025-12-05 18:05:59 +0100

    Add Windows + Logout redirection

---
+++ Views/Editar/ClienteAddWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TVPGestion_IPO.Views
+{
+    public partial class ClienteAddWindow : Window
+    {
+
+        public ClienteViewModel nuevoCliente;
+        public ClienteAddWindow()
+        {
+            InitializeComponent();
+            nuevoCliente = new ClienteViewModel();
+            this.DataContext = nuevoCliente;
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cliente añadido correctamente", "Guardar", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
