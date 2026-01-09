ef87abbaf503ea45ba14db654e4bba76519828c7
luciasc04 <lucia.sanchez73@alu.uclm.es>
2026-01-09 01:39:59 +0100

    ayuda

---
+++ Views/Paginas/Ayuda.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TVPGestion_IPO.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Ayuda.xaml
+    /// </summary>
+    public partial class Ayuda : Page
+    {
+        public Ayuda()
+        {
+            InitializeComponent();
+        }
+    }
+}
